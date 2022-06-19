cd7e43ed10bb630c2fe76b8a96fb5d9083e246b9
Tim Nibert <docz2a@gmail.com>
2022-06-19 19:40:45 +1000

    added in timer resource

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,13 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() { 
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -29,7 +33,8 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
             .add_system(greet_people);
     }
 }
